1e7cb192f850251861b55ccd6fa5cc6a2a63d6d3
itaiglikman <itaig1998@gmail.com>
2025-08-07 10:39:41 +0300

    feat: card css and layout

--- src/Components/NotesArea/NoteCard/NoteCard.tsx
+++ src/Components/NotesArea/NoteCard/NoteCard.tsx
@@ -1,18 +1,17 @@
 import type { Note } from "../../../Models/Note";
 import "./NoteCard.css";
 
-
 export function NoteCard(props: Note) {
     const { id, title, content, created, updated, category, done } = props;
     return (
         <div className="NoteCard" id={id}>
-            <h2>Card</h2>
             {title ? <h3>{title}</h3> : <></>}
-            <div>{content}</div>
+            <div className="card-content">{content}</div>
             <div className="card-date">{created.toLocaleString()}</div>
             {updated ? <div className="card-date">{created.toLocaleString()}</div> : <></>}
             {category ? <div>{category}</div> : <></>}
             {done ? <div>{done}</div> : <></>}
+            <button className="delete-btn" title="Delete">&times;</button>
         </div>
     );
 }
