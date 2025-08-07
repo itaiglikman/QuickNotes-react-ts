742c5df72fd8dcc95e516dc7f7080c7727409378
itaiglikman <itaig1998@gmail.com>
2025-08-07 12:40:55 +0300

    feat: add functinality to delete btn

--- src/Components/NotesArea/NoteCard/NoteCard.tsx
+++ src/Components/NotesArea/NoteCard/NoteCard.tsx
@@ -1,17 +1,36 @@
 import type { Note } from "../../../Models/Note";
 import "./NoteCard.css";
 
-export function NoteCard(props: Note) {
-    const { id, title, content, created, updated, category, done } = props;
+interface NoteCardProps {
+    note: Note;
+    onDelete: (id: string) => void
+}
+
+export function NoteCard({ note, onDelete }: NoteCardProps) {
+    const { id, title, content, created, updated, category, done } = note;
+
+    function handleDelete() {
+        if (confirm(`Are you sure?`)) onDelete(id);
+    }
+
     return (
         <div className="NoteCard" id={id}>
-            {title ? <h3>{title}</h3> : <></>}
+            {title &&
+                <h3>{title}</h3>}
             <div className="card-content">{content}</div>
             <div className="card-date">{created.toLocaleString()}</div>
-            {updated ? <div className="card-date">{created.toLocaleString()}</div> : <></>}
-            {category ? <div>{category}</div> : <></>}
-            {done ? <div>{done}</div> : <></>}
-            <button className="delete-btn" title="Delete">&times;</button>
+            {updated &&
+                <div className="card-date">{created.toLocaleString()}</div>}
+            {category &&
+                <div>{category}</div>}
+            {done &&
+                <div>{done}</div>}
+            <button
+                className="delete-btn"
+                onClick={handleDelete}
+                title="Delete">
+                &times;
+            </button>
         </div>
     );
 }
