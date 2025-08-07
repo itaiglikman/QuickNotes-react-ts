d282f419599f400c4a0aac4dcd1b0272345b70fe
itaiglikman <itaig1998@gmail.com>
2025-08-07 17:57:29 +0300

    create category type and catefories colors Record for a strict use

--- src/Models/Note.ts
+++ src/Models/Note.ts
@@ -4,6 +4,27 @@ export interface Note {
     content: string;
     created: Date;
     updated?: Date;
-    category?: string; //?enum
-    done?: boolean; 
+    category?: CategoryType;
+    done?: boolean;
 }
+
+export type CategoryType =
+  | 'Work'
+  | 'Personal'
+  | 'Ideas'
+  | 'Urgent'
+  | 'Completed'
+  | 'Planning'
+  | 'Research'
+  | 'Low Priority';
+
+export const categoriesColor: Record<CategoryType, string> = {
+  Work: '#4C6EF5',
+  Personal: '#20C997',
+  Ideas: '#FFD43B',
+  Urgent: '#FA5252',
+  Completed: '#40C057',
+  Planning: '#D6336C',
+  Research: '#7048E8',
+  'Low Priority': '#CED4DA',
+};
