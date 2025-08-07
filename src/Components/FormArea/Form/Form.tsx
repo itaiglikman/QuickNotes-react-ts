17df0db5b130ab0ba4b6f79da8797db4d8d66588
itaiglikman <itaig1998@gmail.com>
2025-08-07 10:39:17 +0300

    feat: form css and layout

--- src/Components/FormArea/Form/Form.tsx
+++ src/Components/FormArea/Form/Form.tsx
@@ -1,10 +1,26 @@
 import "./Form.css";
 
+// maybe will be used in modal
+// interface FormType{
+//     noteToUpdate:Note;
+// }
+
 export function Form() {
+
+    function handleSubmit() {
+
+    }
+
     return (
         <div className="Form">
 
-			<p>Form Component</p>
+            <form action="" onSubmit={handleSubmit}>
+                <div className="input-area">
+                    <input type="text" name="title" value={''} placeholder="Title"/>
+                    <textarea name="content" value={''} placeholder="Write here your new note "/>
+                </div>
+                <button>Create</button>
+            </form>
 
         </div>
     );
