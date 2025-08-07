18d7caf09fa242fc6169b9146922c1e067e6c058
itaiglikman <itaig1998@gmail.com>
2025-08-07 11:52:12 +0300

    feat: add form logic- get data from the form add create new note

--- src/Components/FormArea/Form/Form.tsx
+++ src/Components/FormArea/Form/Form.tsx
@@ -1,4 +1,6 @@
+import { useState, type FormEvent } from "react";
 import "./Form.css";
+import type { Note } from "../../../Models/Note";
 
 // maybe will be used in modal
 // interface FormType{
@@ -6,22 +8,53 @@ import "./Form.css";
 // }
 
 export function Form() {
+    const [inputTitle, setInputTitle] = useState<string>('');
+    const [inputContent, setInputContent] = useState<string>('');
+    const [contentErrMsg, setContentErrMsg] = useState('');
 
-    function handleSubmit() {
+    function handleSubmit(event: FormEvent): void {
+        event.preventDefault();
+        validateContent(inputContent);
+        const note = createNewNote(inputContent, inputTitle);
+        console.log(note);
+        setInputTitle('');
+        setInputContent('');
+    }
 
+    function validateContent(content: string): void {
+        if (content === '') {
+            setContentErrMsg('no content');
+            return;
+        }
+        setContentErrMsg('');
+    }
+
+    function createNewNote(content: string, title?: string): Note {
+        return { id: crypto.randomUUID(), title, content, created: new Date() };
     }
 
     return (
         <div className="Form">
-
-            <form action="" onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <div className="input-area">
-                    <input type="text" name="title" value={''} placeholder="Title"/>
-                    <textarea name="content" value={''} placeholder="Write here your new note "/>
+                    <input
+                        type="text"
+                        name="title"
+                        value={inputTitle}
+                        onChange={(e) => setInputTitle(e.target.value)}
+                        placeholder="Title"
+                    />
+                    <textarea
+                        name="content"
+                        value={inputContent}
+                        onChange={(e) => setInputContent(e.target.value)}
+                        placeholder="Write here your new note "
+                    />
+                    {contentErrMsg &&
+                        <div className="err-msg">{contentErrMsg}</div>}
                 </div>
                 <button>Create</button>
             </form>
-
         </div>
     );
 }
