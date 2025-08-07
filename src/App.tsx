08bed125f8f5ea4a9bf34d955474412313e47e90
itaiglikman <itaig1998@gmail.com>
2025-08-07 10:38:13 +0300

    feat: app css and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import { Form } from "./Components/FormArea/Form/Form";
 import { NoteCard } from "./Components/NotesArea/NoteCard/NoteCard";
 import { NotesBoard } from "./Components/NotesArea/NotesBoard/NotesBoard";
+import { SearchBar } from "./Components/SearchArea/SearchBar/SearchBar";
 import type { Note } from "./Models/Note";
 
 function App() {
@@ -11,29 +13,34 @@ function App() {
             title: "Sample Note 1",
             content: "This is the first mock note for testing.",
             created: new Date(),
-            updated: new Date()
         },
         {
             id: "2",
             title: "Sample Note 2",
-            content: "This is the second mock note for testing.",
+            content: "This is the second mock note for testing.This is the second mock note for testing.This is the second mock note for testing.This is the second mock note for testing.This is the second mock note for testing.This is the second mock note for testing.This is the second mock note for testing.This is the second mock note for testing.This is the second mock note for testing.This is the second mock note for testing.This is the second mock note for testing.",
             created: new Date(),
-            updated: new Date()
         },
         {
             id: "3",
             title: "Sample Note 3",
             content: "This is the third mock note for testing.",
             created: new Date(),
-            updated: new Date()
         }
     ];
 
-  return (
-    <div className="App">
-        <NotesBoard notes={notes}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <h1>Quick Notes</h1>
+            </header>
+            <SearchBar />
+            <Form />
+            <NotesBoard notes={notes} />
+            <footer>
+                itai glikman
+            </footer>
+        </div>
+    );
 }
 
 export default App;
