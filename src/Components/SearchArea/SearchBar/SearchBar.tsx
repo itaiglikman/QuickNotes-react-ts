7b0dcdfefd9d8ae9ee6e71bbb7b052f0c494f4e9
itaiglikman <itaig1998@gmail.com>
2025-08-09 14:15:07 +0300

    feat: include categories filter in search bar

--- src/Components/SearchArea/SearchBar/SearchBar.tsx
+++ src/Components/SearchArea/SearchBar/SearchBar.tsx
@@ -1,22 +1,27 @@
 import { useEffect, useState } from "react";
+import { Categories } from "../../Categories/Categories";
 import "./SearchBar.css";
-
 interface SearchBarProps {
     onSearch: (query: string) => void;
+    onFilter: (category: string) => void;
 }
 
-export function SearchBar({ onSearch }: SearchBarProps) {
+export function SearchBar({ onSearch, onFilter }: SearchBarProps) {
 
     const [searchValue, setSearchValue] = useState<string>('');
 
     useEffect(() => {
-      setSearchValue('');
+        setSearchValue('');
     }, [])
-    
+
 
     function handleClick() {
         if (!searchValue.length) return console.log('no search');
         onSearch(searchValue);
+    }
+
+    function handleFilter(category: string) {
+        onFilter(category);
     }
 
     return (
@@ -25,6 +30,7 @@ export function SearchBar({ onSearch }: SearchBarProps) {
                 value={searchValue}
                 onChange={e => setSearchValue(e.target.value)}
                 placeholder="Search notes" />
+            <Categories onSelect={handleFilter} />
             <button onClick={handleClick}>search</button>
         </div>
     );
