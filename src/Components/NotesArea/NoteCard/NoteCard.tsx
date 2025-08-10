48ccd62a1bc406d241404483fb007fcfcfe7c1cc
itaiglikman <itaig1998@gmail.com>
2025-08-10 16:52:50 +0300

    feat: use modal to update the note, improve functionality and readability

--- src/Components/NotesArea/NoteCard/NoteCard.tsx
+++ src/Components/NotesArea/NoteCard/NoteCard.tsx
@@ -1,20 +1,29 @@
 import { categoriesColor, type NoteType } from "../../../Models/Types";
+import { FormModal } from "../../FormArea/Modal/FormModal";
 import "./NoteCard.css";
 
 interface NoteCardProps {
     note: NoteType;
     onDelete: (id: string) => void;
+    onFormSubmit: (note: NoteType) => void;
 }
 
-export function NoteCard({ note, onDelete }: NoteCardProps) {
-    const { id, title, content, created, updated, category, done } = note;
+export function NoteCard({ note, onDelete, onFormSubmit }: NoteCardProps) {
+    const { id, title, content, created, updated, category, done } = { ...note }
 
     function handleDelete() {
         if (confirm(`Are you sure?`)) onDelete(id);
     }
 
     function getCategoryColor(): string | undefined {
-        return category ? categoriesColor[category] : undefined;
+        return category
+            ? categoriesColor[category]
+            : undefined;
+    }
+
+    function handleUpdate(updatedNote: NoteType) {
+        updatedNote.updated = (new Date()).toLocaleString();
+        onFormSubmit(updatedNote);
     }
 
     return (
@@ -24,15 +33,24 @@ export function NoteCard({ note, onDelete }: NoteCardProps) {
             id={id}
         >
             {title && <h3>{title}</h3>}
+
             <div className="card-content">{content}</div>
+
             {category && <div>{category}</div>}
+
             <div className="card-date">created {created}</div>
+
             {updated &&
-                <div className="card-date">{updated}</div>}
+                <div className="card-date">updated {updated}</div>}
+
             {done && <div>{done}</div>}
-            <button className="delete-btn" onClick={handleDelete} title="Delete">
-                &times;
-            </button>
+
+            <div className="btn-area">
+                <FormModal noteToUpdate={note} onUpdate={handleUpdate} />
+                <button className="delete-btn" onClick={handleDelete} title="Delete">
+                    &times;
+                </button>
+            </div>
         </div>
     );
 }
